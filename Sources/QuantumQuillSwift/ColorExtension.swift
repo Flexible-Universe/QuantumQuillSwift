b88ec0990ea89eddf72a1faf14599f815ca5002b
Rolf Warnecke <rwarnecke@Naboo.local>
2023-12-26 13:34:58 +0100

    Update Swift-Package

--- Sources/QuantumQuillSwift/ColorExtension.swift
+++ Sources/QuantumQuillSwift/ColorExtension.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 #if canImport(UIKit)
 import UIKit
 #endif
@@ -81,3 +82,20 @@ public extension UIColor {
     }
 }
 #endif
+
+extension Color {
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
--- Tests/QuantumQuillSwiftTests/ColorExtensionTest.swift
+++ Tests/QuantumQuillSwiftTests/ColorExtensionTest.swift
@@ -6,12 +6,20 @@
 //
 
 import XCTest
+import SwiftUI
+@testable import QuantumQuillSwift
 
 final class ColorExtensionTest: XCTestCase {
 
     func testUIColorFromHex() {
         let testColor = UIColor.init(hexFromString: "#7dbf0d")
         let checkColor = UIColor.init(red: 125.0/255.0, green: 191.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+        XCTAssertEqual(testColor, checkColor)
+    }
+    
+    func testColorFromHex() {
+        let testColor = Color(hex: "#7dbf0d")
+        let checkColor = Color(red: 125.0/255.0, green: 191.0/255.0, blue: 13.0/255.0)
         XCTAssertEqual(testColor, checkColor)
     }
 }
